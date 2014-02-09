0829147d6d8a0868a5c468ee7ff778b6ee47ba3a
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2014-02-08 19:51:13 -0500

    append and remove

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function Mvsb(select){
 
   select.addEventListener('change', function(e){
     if(!select.nextElementSibling || !select.nextElementSibling.classList.contains('mvsb-values')){
-      select.insertAdjacentHTML('afterend', '<ul class="mvsb-values"><li data-value="' + this.value + '">' + this.value + '<a href="#">&times;</a></li></ul>');
+      select.insertAdjacentHTML('afterend', '<ul class="mvsb-values">'+ _createLi(this.value) + '</ul>');
       select.nextElementSibling.addEventListener('click', function(e){
         e.preventDefault();
         if(e.target.tagName === 'A'){
@@ -14,11 +14,75 @@ function Mvsb(select){
       });
     } else {
       if(self.values().indexOf(this.value) === -1){        
-        select.nextElementSibling.insertAdjacentHTML('beforeend', '<li data-value="' + this.value + '">' + this.value + '<a href="#">&times;</a></li>');
+        select.nextElementSibling.insertAdjacentHTML('beforeend', _createLi(this.value));
       }
     }
     this.selectedIndex = 0;
   });
+
+};
+
+Mvsb.prototype.append = function(value){
+  var opt;
+  var opts = this.select.getElementsByTagName('option');
+  for(var i=0; i<opts.length; i++){
+    if(opts[i].value === value){
+      opt = opts[i];
+      break;
+    }
+  }
+
+  if(opt){
+    var ul = this.select.nextElementSibling;
+    if(ul && ul.classList.contains('mvsb-values')){
+      var lis = ul.getElementsByTagName('li');
+
+      var alreadyIn = false;
+      for(var i=0; i<lis.length; i++){
+        if(lis[i].getAttribute('data-value') === value){
+          alreadyIn = true;
+          break;
+        };
+      }
+
+      if(!alreadyIn){
+        ul.insertAdjacentHTML('beforeend', _createLi(value));
+      }
+    } else {
+      this.select.insertAdjacentHTML('afterend', '<ul class="mvsb-values">'+ _createLi(value) + '</ul>');      
+    }
+  }
+};
+
+Mvsb.prototype.remove = function(value){
+  var opt;
+  var opts = this.select.getElementsByTagName('option');
+  for(var i=0; i<opts.length; i++){
+    if(opts[i].value === value){
+      opt = opts[i];
+      break;
+    }
+  }
+
+  if(opt){
+    opt.parentNode.removeChild(opt);
+
+    var ul = this.select.nextElementSibling;
+    if(ul && ul.classList.contains('mvsb-values')){
+      var lis = ul.getElementsByTagName('li');
+      var li;
+      for(var i=0; i<lis.length; i++){
+        if(lis[i].getAttribute('data-value') === value){
+          li = lis[i];
+          break;
+        };
+      }
+
+      if(li){
+        ul.removeChild(li);
+      }
+    }
+  }  
 
 };
 
@@ -35,6 +99,13 @@ Mvsb.prototype.values = function(){
 
   return values;
 };
+
+
+
+function _createLi(value){
+  return '<li data-value="' + value + '">' + value + '<a href="#">&times;</a></li>';
+};
+
 
 if (typeof module !== 'undefined' && module.exports){
   module.exports = Mvsb;
